7da479c768964b6dfbda95de04cba32b4862d40b
velmeshevlab <114241370+velmeshevlab@users.noreply.github.com>
2024-07-26 16:44:55 -0400

    Update sex_specific_analysis.R

--- sex_specific_analysis.R
+++ sex_specific_analysis.R
@@ -1,7 +1,7 @@
 #######################################
 Excitatry neurons
 #######################################
-ExNeu = readRDS(file = "ExNeu_AL.R"DS)
+ExNeu = readRDS(file = "ExNeu_AL.RDS")
 #isolate male cells and compress lineages
 cells = rownames(colData(ExNeu)[colData(ExNeu)$sex == "Male",])
 cds.male = ExNeu[,cells]
@@ -84,24 +84,82 @@ write.table(subset(cds_pr_test_res, q_value < 0.05), paste("pt_DGE_", lineage,"_
 
 #get sex-enriched lineage genes
 exneu_sex = combine_objects(cds.male, cds.female, "_Male", "_Female")
-cds_new = combine_objects(exneu_sex, in_sex, "", "")
-get_sex_enriched_genes(cds_new, factor = 1.2, window_ratio = 0.01, I = 0.1)
+get_sex_enriched_genes(exneu_sex, factor = 1.2, window_ratio = 0.01, I = 0.1)
                        
 #classify genes
 meta = read.table("meta.txt", row.names=1, header=T, sep="\t", check.names = F)
 lineages = c("L2_3", "L4", "L5_6_IT", "SP")
 for(lineage in lineages){
 print(lineage)
-res = get_max_age_v2(cds_new, meta = meta, lineage = lineage, start = 1334)
+res = get_max_age_v2(ExNeu, meta = meta, lineage = lineage, start = 1334)
 write.table(t(res), paste0("mod_", lineage,".txt"), sep = "\t", quote = F)
 }
 lineages = c("L5", "L6")
 for(lineage in lineages){
 print(lineage)
-res = get_max_age_v2(cds_new, meta = meta, lineage = lineage, start = 1320)
+res = get_max_age_v2(ExNeu, meta = meta, lineage = lineage, start = 1320)
 write.table(t(res), paste0("mod_", lineage,".txt"), sep = "\t", quote = F)
 }
 
 #######################################
 Interneurons
 #######################################
+IN = readRDS(file = "IN.RDS")
+#isolate male cells and compress lineages
+cells = rownames(colData(IN)[colData(IN)$sex == "Male",])
+cds.male = IN[,cells]
+cds.male = get_lineage_object(cds.male, start = 203)
+cds.male <- compress_lineages_v2(cds.male, start = 203, cores = 24)
+#female
+cells = rownames(colData(IN)[colData(IN)$sex == "Female",])
+cds.female = IN[,cells]
+cds.female = get_lineage_object(cds.female, start = 203)
+cds.female <- compress_lineages_v2(cds.female, start = 203, cores = 24)
+
+#Run Moran's I test
+#male
+factor = 0.05
+N = 10000
+lineages = names(cds.male@lineages)
+for(lineage in lineages){
+options(mc.cores = 1)
+print(lineage)
+cds.sub = get_lineage_object(cds.male, lineage, 203, N = N)
+data = counts(cds.sub)
+rows=rownames(data)[rowSums(data> 0) > factor*ncol(data)]
+cds.sel = cds.sub[rows]
+print(paste0("Testing ", ncol(cds.sel)," cells and ", nrow(cds.sel), " genes"))
+cds_pr_test_res <- monocle3:::graph_test(cds.sel, neighbor_graph="principal_graph", verbose = T, cores = 24)
+save(cds_pr_test_res, file = paste("Moran_", lineage,"_Male.R", sep = ""))
+write.table(subset(cds_pr_test_res, q_value < 0.05), paste("pt_DGE_", lineage,"_Male.txt", sep = ""), sep ="\t", quote = F)
+}
+  
+#female
+factor = 0.05
+N = 10000
+lineages = names(cds.female@lineages)
+for(lineage in lineages){
+options(mc.cores = 1)
+print(lineage)
+cds.sub = get_lineage_object(cds.female, lineage, 203, N = N)
+data = counts(cds.sub)
+rows=rownames(data)[rowSums(data> 0) > factor*ncol(data)]
+cds.sel = cds.sub[rows]
+print(paste0("Testing ", ncol(cds.sel)," cells and ", nrow(cds.sel), " genes"))
+cds_pr_test_res <- monocle3:::graph_test(cds.sel, neighbor_graph="principal_graph", verbose = T, cores = 24)
+save(cds_pr_test_res, file = paste("Moran_", lineage,"_Female.R", sep = ""))
+write.table(subset(cds_pr_test_res, q_value < 0.05), paste("pt_DGE_", lineage,"_Female.txt", sep = ""), sep ="\t", quote = F)
+}
+
+in_sex = combine_objects(cds.male, cds.female, "_Male", "_Female")
+get_sex_enriched_genes(in_sex, factor = 1.2, window_ratio = 0.01, I = 0.1)
+
+#classify genes
+meta = read.table("meta.txt", row.names=1, header=T, sep="\t", check.names = F)
+lineages = names(IN@lineages)
+for(lineage in lineages){
+print(lineage)
+res = get_max_age_v2(IN, meta = meta, lineage = lineage, start = 203)
+write.table(t(res), paste0("mod_", lineage,".txt"), sep = "\t", quote = F)
+}
+
